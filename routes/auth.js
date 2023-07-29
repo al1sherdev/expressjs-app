3d4d23116602154934fb3bb421d9b748178c7171
al1sherdev <tangirbergenovalisher34@gmail.com>
2023-07-29 21:51:08 +0500

    auth || ui

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,26 @@ import { Router } from "express";
 const router = Router()
 
 router.get('/signup', (req, res) => {
-    res.render('signup')
+    res.render('signup', {
+        title: 'SignUp | Ali Shop',
+    })
+})
+
+router.post('/signup', (req, res) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+
+router.get('/signin', (req, res) => {
+    res.render('signin', {
+        title: 'SignIn | Ali Shop',
+    })
+})
+
+router.post('/signin', (req, res) => {
+    console.log(req.body)
+    res.redirect('/')
 })
 
 
--- routes/product.js
+++ routes/product.js
@@ -2,19 +2,30 @@ import { Router } from "express";
 const router = Router()
 
 router.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: 'Ali Shop'
+    })
 })
 
 router.get('/products', (req, res) => {
-    res.render('products')
+    res.render('products', {
+        title: 'Products | Ali Shop',
+        isProducts: true
+    })
 })
 
 router.get('/add', (req, res) => {
-    res.render('add')
+    res.render('add', {
+        title: 'Add product | Ali Shop',
+        isAdd: true
+    })
 })
 
 router.get('/support', (req, res) => {
-    res.render('support')
+    res.render('support', {
+        title: 'Support || Ali Shop',
+        isSupport: true
+    })
 })
 
 
