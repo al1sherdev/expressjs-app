7548e451bcefc57b4379214646d31740bf3c3928
al1sherdev <tangirbergenovalisher34@gmail.com>
2023-07-29 17:44:12 +0500

    expressjs-app start

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import {create, engine} from 'express-handlebars'
+
+const app = express()
+
+const hbs = create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', './views')
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+const PORT = process.env.PORT || 4100 
+app.listen(4100, () => console.log(`Server is starting: ${PORT}`)) 
